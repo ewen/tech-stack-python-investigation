96bfe9a4fa265d4ffcbc678611390f2025818f88
Ewen Cumming <ewen@mediasuite.co.nz>
2017-03-08 13:09:21 +1300

    Add Django

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tech-stack-python-investigation
+
+## Installation
+`docker run -e POSTGRES_USER="books" -e POSTGRES_PASSWORD="books" -e POSTGRES_DB="books" -p 54321:5432 -d postgres`
+
---
+++ books/models.py
@@ -0,0 +1,21 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=100)
