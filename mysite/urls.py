52cda493d71162e764c0fec599627e710df95b78
Ersin Buckley <ersin@mediasuite.co.nz>
2017-03-08 14:14:18 +1300

    add djangorestframework and initial endpoints

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,35 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from books.models import Book, Author
+
+from rest_framework import routers, serializers, viewsets
+
+class BooksSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('author', 'title', 'pub_date')
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
+
+class AuthorsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name']
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorsSerializer
+
+router = routers.DefaultRouter()
+router.register(r'books', BookViewSet)
+router.register(r'authors', AuthorViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^books/', include('books.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
