102b5c64dc2caa38872811d891d1be525ae0c0af
jonathanh <jonforest@gmail.com>
2017-05-11 09:28:05 +1200

    Handle errors in the response to a POST/PATCH and add them to the model in a way that can be rendered out on the client

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -12,8 +12,17 @@ export default DS.JSONAPIAdapter.extend({
     }
   }),
   host: ENV.apiHost,
-  buildURL: function(type, id, record) {
-     //call the default buildURL and then append a slash
-     return this._super(type, id, record) + '/';
-   }
-});
+  buildURL: function (type, id, record) {
+    // call the default buildURL and then append a slash
+    return this._super(type, id, record) + '/'
+  },
+  handleResponse: function (status, headers, payload) {
+    if (status >= 400 && status < 500 && payload.errors) {
+      if (payload.errors[0].code === 'AUTHORIZATION_REQUIRED') {
+        return get(this, 'session').logout()
+      }
+      return new DS.InvalidError(payload.errors)
+    }
+    return this._super(...arguments)
+  }
+})
